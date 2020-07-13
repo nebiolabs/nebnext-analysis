526b7a981fe4c9ee16ee1247f6c532679384d58d
Timur Shtatland <shtatland@neb.com>
2020-07-13 15:39:28 -0400

    Add test script for single end adapter trimming

---
+++ flexbar/test/test_single_end.sh
@@ -0,0 +1,181 @@
+#!/usr/bin/env bash
+
+read -d '' usage <<'EOF'
+
+test.sh - runs tests for flexbar adapter trimming.
+
+DESCRIPTION
+
+flexbar must be installed and present in the path to run these
+tests. Exit status = 0 is all tests succeeded, and non-zero
+otherwise.
+
+Writes files with test details in the current dir:
+test_flexbar.tsv
+test_flexbar.log
+
+USAGE
+
+test.sh
+
+EXAMPLES OF USAGE
+
+# Run tests and print ok if all tests succeeded:
+test.sh && echo 'ok' || echo 'not ok'
+
+# View the detailed results:
+
+cat test_flexbar.tsv | column -t -s$'\t'
+
+# Prints this table. See test_descriptions.tsv for details. 
+     
+library                            is_ok
+r1_ins_1_28_tso_full_g5_rev        ok
+r1_ins_1_40_ilmn_13_fwd            ok
+r1_ins_1_40_tso_full_g5_rev        ok
+r1_ins_1_59_ilmn_13_fwd            ok
+r1_tso_full_g4_fwd_1_43_ins_44_76  ok
+r1_tso_full_t29_fwd_1_66_ins       ok
+r1_tso_full_t39_fwd_1_76_no_ins    ok
+r2_tso_full_g4_fwd_1_43_ins_44_76  ok
+r2_tso_full_t39_fwd_1_76_no_ins    not ok
+
+Note: In the original test script for PE sequences, all tests pass. Here,
+1 test is expected to fail: r2_tso_full_t39_fwd_1_76_no_ins . It is expected
+to fail - the trimming is actually correct due to absence of read2.
+Here are the details.
+
+GCTAATCATTGCAAGCAGTGGTATCAACGCAGAGTACTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT 3p tso_full_t39_fwd
+GCTAATCATTGCAAGCAGTGGTATCAACGCAGAGTACTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT 3p r2
+
+r1 has no adapter and no overlap with r1.
+r1 is mapped to transcript.
+r2 should be fully trimmed and discarded (without trimming, with soft-clipping, it maps to another transcript, it is a FP)
+
+OK, unlike PE reads, where r1 is discarded bc r2 is fully trimmed and thus
+discarded, in this case r1 is the only SE read (no r2), and it is not trimmed
+and not discarded, as expected.
+
+
+EOF
+
+# export usage='foo'
+
+if [[ "$#" -ne "0" ]] ; then
+    echo "${usage}"
+    exit 1
+fi
+
+export script_dir=`dirname $0`
+
+export adapter_dir=${script_dir}/fixtures/adapters
+
+export fastq_dir=${script_dir}/fixtures/in
+
+export exp_dir=${script_dir}/fixtures/exp
+
+export obs_dir=${script_dir}/fixtures/obs
+
+mkdir -p ${obs_dir}
+
+# remove old test results:
+rm -f ${obs_dir}/*.fastq ${obs_dir}/*.log
+
+# optimal run time, higher numbers have no effect:
+
+export num_threads=4
+
+
+# Run flexbar 2x: right trim illumina, then left trim tso adapters (as
+# of this flexbar version, these two steps cannot be
+# combined). Compare expected and observed output fastq files, write
+# ok/not ok test results into tsv file, and diagnostic info into log
+# file.
+
+cat ${script_dir}/fixtures/test_descriptions.tsv | tail -n +2 | cut -f1 | \
+    perl -lne '
+BEGIN {
+        @fields = qw( library is_ok );
+        print join "\t", @fields;
+
+}
+@cmds = ();
+%val = ();
+$status = 0;
+$val{library} = $_;
+
+$cmd = join q{ },
+        qq{flexbar},
+        qq{--reads $ENV{fastq_dir}/$val{library}_1.fastq},
+        qq{--stdout-reads},
+        qq{--adapters $ENV{adapter_dir}/tso_g_wo_hp.fasta},
+        qq{--adapter-trim-end LEFT},
+        qq{--adapter-revcomp ON},
+        qq{--adapter-revcomp-end RIGHT},
+        qq{--htrim-left GT},
+        qq{--htrim-right CA},
+        qq{--htrim-min-length 3},
+        qq{--htrim-max-length 5},
+        qq{--htrim-max-first},
+        qq{--htrim-adapter},
+        qq{--min-read-length 2},
+        qq{--threads $ENV{num_threads}},
+
+        qq{|},
+        qq{flexbar},
+        qq{--reads -},
+        qq{--target $ENV{obs_dir}/$val{library}},
+        qq{--adapters $ENV{adapter_dir}/ilmn_20_2_seqs.fasta},
+        qq{--adapter-trim-end RIGHT},
+        qq{--min-read-length 2},
+        qq{--threads $ENV{num_threads}},
+
+        qq{;},
+        qq{mv flexbarOut.log $ENV{obs_dir}/$val{library}.1.log},
+        qq{;},
+        qq{mv $ENV{obs_dir}/$val{library}.log $ENV{obs_dir}/$val{library}.2.log},
+        ;
+
+print {*STDERR} "$cmd";
+system $cmd;
+
+for $mate (1) {
+        # temp patch for the case where in one of the previous steps,
+        # flexbar discarded all the reads in a file, so subsequent
+        # flexbar calls for empty inputs do not create output files:
+
+        $cmd = qq{touch $ENV{obs_dir}/$val{library}.fastq};
+        print {*STDERR} "$cmd";
+        system $cmd;
+
+        $cmd = join q{ },
+                qq{diff},
+                qq{$ENV{exp_dir}/$val{library}_${mate}.fastq},
+                qq{$ENV{obs_dir}/$val{library}.fastq},
+                qq{>/dev/null};
+        print {*STDERR} "$cmd";
+        system $cmd and $status++;
+}
+$val{is_ok} = $status ? q{not ok} : q{ok};
+print join "\t", @val{ @fields };
+' \
+         1>test_flexbar.tsv \
+         2>test_flexbar.log
+
+num_exp_tests=`tail -n +2 ${script_dir}/fixtures/test_descriptions.tsv | wc -l`
+
+num_obs_tests=`tail -n +2 test_flexbar.tsv | wc -l`
+
+if [[ ${num_exp_tests} -ne ${num_obs_tests} ]] ; then
+    echo "expected to run ${num_exp_tests} tests, got ${num_obs_tests}"
+    exit 2
+fi
+
+num_failed_tests=`grep -c 'not ok' test_flexbar.tsv`
+
+if [[ ${num_failed_tests} -ne 0 ]] ; then
+    echo "${num_failed_tests} failed tests"
+    exit 2
+fi
+
+exit 0
